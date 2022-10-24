b2b1b24591191ee8d81ebf7b6db7776ea9499a64
nat-418 <93013864+nat-418@users.noreply.github.com>
2022-10-24 21:15:47 +0200

    Add setup() and comments

--- lua/tcl/init.lua
+++ lua/tcl/init.lua
@@ -1,13 +1,16 @@
 local M = {}
 
+-- Check Tcl syntax and populate the quickfix list with errors.
 M.nagelfar = function(target)
+  -- Save pre-existing settings to restore at the end.
   local old_makeprg = vim.bo.makeprg
   local old_pattern = vim.bo.errorformat
 
+  -- This `-H` flag is necessary to prepend errors with line numbers.
   vim.bo.makeprg = 'nagelfar -H'
 
-  -- Parses `$ nagelfar -H $target` output
-  local pattern = table.concat({
+  -- Parses `$ nagelfar -H $target` output for `:make`.
+  vim.bo.errorformat = table.concat({
     '%E%f: %l: %t %m',
     '%Z%f%.%#',
     '%C %.%m',
@@ -15,18 +18,19 @@ M.nagelfar = function(target)
     '%C%.%#'
   }, ',')
 
-  vim.bo.errorformat = pattern
+  vim.cmd.make(target) -- Populate the quickfix list with results.
 
-  vim.cmd.make(target)
-
+  -- Restore pre-existing settings saved at the beginning.
   vim.bo.makeprg     = old_makeprg
   vim.bo.errorformat = old_pattern
 
   return true
 end
 
-vim.api.nvim_create_user_command(
-  'Nagelfar',
-  function(args) M.nagelfar(args.args) end,
-  {nargs = 1}
-)
+M.setup = function()
+  vim.api.nvim_create_user_command(
+    'Nagelfar',
+    function(args) M.nagelfar(args.args) end,
+    {nargs = 1}
+  )
+end
